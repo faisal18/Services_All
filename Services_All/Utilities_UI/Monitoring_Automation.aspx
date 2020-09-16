1b307025b1fed21c835b53ef5ea9e45c88a0bbd2
M.Faisal Ansari <faisalansari.m@hotmail.com>
2020-09-16 12:19:07 +0400

    FA: Small UI for automation monitoring

---
+++ Services_All/Utilities_UI/Monitoring_Automation.aspx
@@ -0,0 +1,149 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Monitoring_Automation.aspx.cs" Inherits="Services_All.Utilities_UI.Monitoring_Automation" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <h1>Monitoring Automation</h1>
+
+    <div>
+        <asp:Label runat="server" Text="Client"></asp:Label>
+    </div>
+    <div>
+        <asp:DropDownList runat="server" ID="ddl_client" AutoPostBack="true" OnSelectedIndexChanged="ddl_client_SelectedIndexChanged"
+            DataTextField="Text" DataValueField="Value">
+        </asp:DropDownList>
+    </div>
+
+    <div>
+        <asp:Label runat="server" Text="Products">
+        </asp:Label>
+    </div>
+    <div>
+        <asp:DropDownList runat="server" ID="ddl_products" AutoPostBack="true" OnSelectedIndexChanged="ddl_products_SelectedIndexChanged"
+            DataTextField="Text" DataValueField="Value">
+        </asp:DropDownList>
+    </div>
+
+    <div>
+        <asp:Label runat="server" Text="Services">
+        </asp:Label>
+    </div>
+    <div>
+        <asp:DropDownList runat="server" ID="ddl_services" AutoPostBack="true" OnSelectedIndexChanged="ddl_services_SelectedIndexChanged"
+            DataTextField="Text" DataValueField="Value">
+        </asp:DropDownList>
+    </div>
+
+    <div>
+        <asp:Label runat="server" Text="Logs"></asp:Label>
+    </div>
+    <div>
+        <asp:DropDownList runat="server" ID="ddl_logs" AutoPostBack="true"
+            DataTextField="Text" DataValueField="Value">
+        </asp:DropDownList>
+    </div>
+
+
+    <div>
+        <fieldset>
+            <legend>Modules</legend>
+   <%-- <div>
+        <asp:Label runat="server" Text="Select Module"></asp:Label>
+    </div>--%>
+    <div>
+        <asp:DropDownList runat="server" ID="ddl_operations" AutoPostBack="true" OnSelectedIndexChanged="ddl_operations_SelectedIndexChanged">
+            <asp:ListItem Selected="True" Value="Service">Service</asp:ListItem>
+            <asp:ListItem Value="Log">Log</asp:ListItem>
+            <asp:ListItem Value="Server">Server</asp:ListItem>
+        </asp:DropDownList>
+    </div>
+            </fieldset>
+        </div>
+
+    <div>
+        <asp:Panel runat="server" Visible="true" ID="pnl_service">
+            <div>
+                <fieldset>
+                    <legend>Operations</legend>
+                    <div>
+                        <asp:RadioButtonList runat="server" ID="rdl_servicesoperation">
+                            <asp:ListItem Value="start">Start</asp:ListItem>
+                            <asp:ListItem Value="stop">Stop</asp:ListItem>
+                            <asp:ListItem Value="status">Status</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </div>
+                </fieldset>
+            </div>
+
+    <div>
+        <asp:Button runat="server" ID="btn_service" Text="Submit" OnClick="btn_service_Click" />
+    </div>
+    </asp:Panel>
+    </div>
+
+    <div>
+        <asp:Panel runat="server" Visible="false" ID="pnl_logs">
+            <div>
+                <fieldset>
+                    <legend>Operations</legend>
+                    <div>
+                        <asp:Label runat="server" Text="Number of lines"></asp:Label>
+                        <asp:TextBox runat="server" ID="txt_logs_numberoflines"></asp:TextBox>
+                    </div>
+                    <div>
+                        <asp:RadioButtonList runat="server" ID="rdl_logs">
+                            <asp:ListItem Selected="True" Value="head">Head</asp:ListItem>
+                            <asp:ListItem Value="tail">Tail</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </div>
+                </fieldset>
+            </div>
+
+            <div>
+                <asp:Button runat="server" ID="btn_logs" Text="Submit" OnClick="btn_logs_Click" />
+            </div>
+
+        </asp:Panel>
+    </div>
+
+    <div>
+        <asp:Panel runat="server" Visible="false" ID="pnl_server">
+            <div>
+                <fieldset>
+                    <legend>Operations</legend>
+                    <div>
+                        <asp:RadioButtonList runat="server" ID="rdl_server">
+                            <asp:ListItem Selected="True" Value="CPU">CPUUsage</asp:ListItem>
+                            <asp:ListItem Value="Memory">MemoryUtilization</asp:ListItem>
+                            <asp:ListItem Value="Disk">DiskSpace</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </div>
+                    <div>
+                        <asp:Button runat="server" ID="btn_server" Text="Submit" OnClick="btn_server_Click" />
+                    </div>
+                </fieldset>
+            </div>
+        </asp:Panel>
+    </div>
+
+
+    <div>
+        <asp:Panel runat="server" Visible="false" ID="pnl_insert">
+            <div>
+                <asp:Button runat="server" ID="btn_submit" Text="Submit" OnClick="btn_submit_Click" />
+            </div>
+        </asp:Panel>
+    </div>
+
+
+
+    <div id="ResultBox">
+        <textarea runat="server" id="txt_richbox" style="resize: none; height: 196px; width: 921px; margin: 0px;" readonly="readonly"></textarea>
+    </div>
+    <div>
+        <asp:Label runat="server" ID="lbl_error"></asp:Label>
+    </div>
+
+
+
+</asp:Content>
---
+++ Services_All/Utilities_UI/Monitoring_Automation.aspx.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Services_All.Utilities_UI
+{
+    public partial class Monitoring_Automation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                SetObj();
+                if (!Page.IsPostBack)
+                {
+                   
+                    Load_DDL();
+                }
+            }
+            catch (Exception ex)
+            {
+                lbl_error.Text = ex.InnerException + ex.Message;
+            }
+
+        }
+
+        private ExtraClasses.Monitoring_Automation obj;
+        private void SetObj()
+        {
+            try
+            {
+                ExtraClasses.Monitoring_Automation obj = new ExtraClasses.Monitoring_Automation();
+                this.obj = obj;
+            }
+            catch (Exception ex)
+            {
+                lbl_error.Text = ex.Message;
+                Logger.Info(ex);
+            }
+        }
+
+        protected void ddl_operations_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ddl_operations.SelectedValue.ToLower() == "service")
+                {
+                    pnl_server.Visible = false;
+                    pnl_logs.Visible = false;
+                    pnl_service.Visible = true;
+                }
+                else if (ddl_operations.SelectedValue.ToLower() == "log")
+                {
+                    pnl_server.Visible = false;
+                    pnl_logs.Visible = true;
+                    pnl_service.Visible = false;
+                }
+                else if (ddl_operations.SelectedValue.ToLower() == "server")
+                {
+                    pnl_server.Visible = true;
+                    pnl_logs.Visible = false;
+                    pnl_service.Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                lbl_error.Text = ex.InnerException + ex.Message;
+            }
+        }
+
+        protected void Load_DDL()
+        {
+            try
+            {
+                DataTable dt_client = obj.Get_Clients();
+
+                ddl_client.DataSource = dt_client;
+                ddl_client.DataValueField = "Id";
+                ddl_client.DataTextField = "Description";
+                ddl_client.DataBind();
+
+                DataTable dt_product = obj.Get_Products(int.Parse(ddl_client.Items[0].Value));
+                ddl_products.DataSource = dt_product;
+                ddl_products.DataValueField = "Id";
+                ddl_products.DataTextField = "Description";
+                ddl_products.DataBind();
+
+
+                DataTable dt_service = obj.Get_Services(int.Parse(ddl_products.Items[0].Value));
+                ddl_services.DataSource = dt_service;
+                ddl_services.DataValueField = "Id";
+                ddl_services.DataTextField = "Description";
+                ddl_services.DataBind();
+
+
+                DataTable dt_Logs = obj.Get_Logs(int.Parse(ddl_services.Items[0].Value));
+                if (dt_Logs != null)
+                    if (dt_Logs.Rows.Count > 0)
+                    {
+                        ddl_logs.DataSource = dt_Logs;
+                        ddl_logs.DataValueField = "Id";
+                        ddl_logs.DataTextField = "Description";
+                        ddl_logs.DataBind();
+                    }
+            }
+            catch (Exception ex)
+            {
+                lbl_error.Text = ex.Message;
+                Logger.Info(ex);
+            }
+        }
+        protected void ddl_client_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ddl_products.DataSource = obj.Get_Products(int.Parse(ddl_client.SelectedValue));
+                ddl_products.DataBind();
+                ddl_services.DataSource = obj.Get_Services(int.Parse(ddl_products.SelectedValue));
+                ddl_services.DataBind();
+                ddl_logs.DataSource = obj.Get_Logs(int.Parse(ddl_services.SelectedValue));
+                ddl_logs.DataBind();
+            }
+            catch (Exception ex)
+            {
+                lbl_error.Text = ex.Message;
+                Logger.Info(ex);
+            }
+        }
+        protected void ddl_products_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ddl_services.DataSource = obj.Get_Services(int.Parse(ddl_products.SelectedValue));
+                ddl_services.DataBind();
+                if (ddl_services.SelectedValue.Length > 0)
+                {
+                    ddl_logs.DataSource = obj.Get_Logs(int.Parse(ddl_services.SelectedValue));
+                }
+                else
+                {
+                    ddl_logs.DataSource = null;
+                    ddl_logs.DataBind();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                lbl_error.Text = ex.Message;
+                Logger.Info(ex);
+            }
+        }
+        protected void ddl_services_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ddl_logs.DataSource = obj.Get_Products(int.Parse(ddl_services.SelectedValue));
+                ddl_logs.DataBind();
+            }
+            catch (Exception ex)
+            {
+                lbl_error.Text = ex.Message;
+                Logger.Info(ex);
+            }
+        }
+
+        protected void btn_service_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string operation = rdl_servicesoperation.SelectedValue.ToLower();
+                txt_richbox.InnerText = obj.Service(operation, int.Parse(ddl_services.SelectedValue));
+
+            }
+            catch (Exception ex)
+            {
+                lbl_error.Text = ex.Message;
+                Logger.Info(ex);
+            }
+        }
+        protected void btn_logs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string operation = rdl_logs.SelectedValue.ToLower();
+                string lines = txt_logs_numberoflines.Text;
+                txt_richbox.InnerText = obj.Log(operation, lines, int.Parse(ddl_logs.SelectedValue));
+
+
+            }
+            catch (Exception ex)
+            {
+                lbl_error.Text = ex.Message;
+                Logger.Info(ex);
+            }
+        }
+        protected void btn_server_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string operation = rdl_server.SelectedValue.ToLower();
+                txt_richbox.InnerText = obj.Server(operation, int.Parse(ddl_services.SelectedValue));
+
+            }
+            catch (Exception ex)
+            {
+                lbl_error.Text = ex.Message;
+                Logger.Info(ex);
+            }
+        }
+
+        protected void btn_submit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                lbl_error.Text = ex.Message;
+                Logger.Info(ex);
+            }
+        }
+
+
+    }
+}
